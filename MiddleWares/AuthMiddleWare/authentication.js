8d14fe8202392c17ac0151a8c726f7b0c43b7bef
Malik Saad Naeem <156428364+maliksaadnaeem937@users.noreply.github.com>
2025-04-08 18:13:34 +0500

    console removed

--- MiddleWares/AuthMiddleWare/authentication.js
+++ MiddleWares/AuthMiddleWare/authentication.js
@@ -9,8 +9,8 @@ const isAuthenticated = (req, res, next) => {
 
   // const accessToken = authHeader?.split(" ")[1];
 
-  console.log("inside authentication.js");
-  console.log(req.headers);
+  // console.log("inside authentication.js");
+  // console.log(req.headers);
   const refreshToken = req.cookies?.refreshToken || "";
   const accessToken = req.cookies?.accessToken || "";
   if (!accessToken) {
